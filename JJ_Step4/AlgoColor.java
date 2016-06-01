2d06c54803cbf37852104b6de0be0a76f45386fd
Deviljushiro <jean.miquel@live.fr>
2016-06-01 15:39:39 +0200

    Finished also

--- JJ_Step4/AlgoColor.java
+++ JJ_Step4/AlgoColor.java
@@ -1,1 +1,10 @@
-Je voudrais un kebab sauce algérienne avec un peu de frites mais pas trop parce que c'est quand même gras... 
+Je voudrais un kebab sauce algérienne avec un peu de frites mais pas trop
+Je trouve ça gras quand même
+
+Bon par contre j ai pas vu l épisode 6 julia donc tu vas éviter de me spoil merci.
+
+Non sérieux je rigole pas.
+
+Si tu regardes mon code c est que tu as cru que j avais fini alors que le prof a même pas donné les consignes.
+
+PS : j ai eu 94 % au projet voltaire kestuvafer
