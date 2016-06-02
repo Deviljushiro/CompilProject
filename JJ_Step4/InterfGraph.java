b928b49bc86fe2a0ccb71b42f08fe943c48af076
Julia Favrel <julia.favrel@live.fr>
2016-06-02 15:10:49 +0200

    add deleteInterf()
    Début de toColor

--- JJ_Step4/InterfGraph.java
+++ JJ_Step4/InterfGraph.java
@@ -8,13 +8,16 @@ class InterfGraph {
   private ArrayList<String> vertices; //String list of each vertex name : ("a","b",...)
   private ArrayList<String> interf;   //List of interferences between vertices (pattern : "vertex1-vertex2")
   private ArrayList<String> pref;     //List of preferences between vertices (pattern : "vertex1-vertex2")
-  
+  private ArrayList<String> toColor; //String list of each vertex name to color, without spilled vertex: ("a","b",...)
+  private int degree; //The degree of the InternGraph to respect
+
   /**Constructor**/
   InterfGraph (ArrayList<String> v, ArrayList<String> i, ArrayList<String> p) {
 
   	this.vertices = v;
   	this.interf = i;
   	this.pref = p;
+    this.degree = k;
   }
 
   
@@ -67,34 +70,92 @@ class InterfGraph {
 	return max;
   }
 
+    /**
+  * @param None, just use the intern InterfGraph
+  * @return int, the the minimum degree of the graph
+  **/
+  String minDegree () {
+  
+  int min = this.getDegree(this.interf.get(0)); //Initialize the first degree's vertex as the min
+
+  for (int i=1;i<=this.interf.size();i++) {
+
+    if (this.getDegree(this.interf.get(i)) < min) {
+
+      min = this.getDegree(this.interf.get(i)); //This is the new minimum degree  
+    }
+  }
+  return min;
+  }
+
 
   /**
   * @param None, just use the intern InterfGraph
-  * @return int, the minimum degree of the graph
+  * @return String, the vertex with the minimum degree of the graph
   **/
-  int minDegree () {
+  String minDegreeVertex () {
 	
 	int min = this.getDegree(this.interf.get(0)); //Initialize the first degree's vertex as the min
+  String verticeMin = this.getVertex(this.interf.get(0)); //Initialize the first vertex as the min degree's
 
 	for (int i=1;i<=this.interf.size();i++) {
 
 		if (this.getDegree(this.interf.get(i)) < min) {
 
 			min = this.getDegree(this.interf.get(i));	//This is the new minimum degree	
+      verticeMin = this.getVertex(this.interf.get(i));  //This is the new minimum degree
 		}
 	}
-	return min;
+	return verticeMin;
+  }
+
+  /**
+  * @param ArrayList<String> interferences, lsit to modify
+  * @param String vertex, vertex that we have to delete it interferences
+  **/
+  void deleteInterf (ArrayList<String> interferences, String vertex){
+      for (int i=0;i<interferences.size();i++) {
+
+        String [] splittedEdge = interferences.get(i).split("-"); //Split the "vertex1-vertex2" in splittedEdge[vertex1,vertex2]
+    
+        if ((splittedEdge[0] == vertex)||(splittedEdge[1] == vertex)) { //If the vertex is in the edge then remove this link of interferences
+          interferences.remove(i);
+        } 
+      }    
+  }
+
+  /**
+  * @param None, just use the intern InterfGraph
+  * @return ArrayList<String> vertexToColor, the list of vertices to color
+  **/
+  ArrayList<String> toColor ( int k) {
+
+  ArrayList<String> vertexToColor; //List to return, with all the vertices to color, without them to spill
+  ArrayList<String> interferences = this.interf.clone(); //Copy of the initial list of interferences 
+  ArrayList<String> vertices = this.vertices.clone(); //Copy of the initial list of vertices
+
+    while( !(vertices.empty()) ){
+
+      vertex = vertices[0]; //Initialize the first vertice
+
+      while(vertex)
+        vertexToColor.add(vertex()); 
+        deleteInterf(interferences, vertex)
+    }
+
+
+  
   }
 
 
   /**
   * @param int colorNumber, 
   * @return boolean, True if the graph needs to be spilled, False else
- 
+  **/
   boolean needSpill (int colorNumber) {
 
 	return (this.maxDegree() >
 
   }
-  **/
+  
 }
